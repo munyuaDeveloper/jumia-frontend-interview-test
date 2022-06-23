8d6987d079ec78463b5dc81cc9c39c76158d9cf1
munyuaDeveloper <munyuapeter06@gmail.com>
2022-06-23 12:10:45 +0300

    added mobile view cypress tests

--- cypress/e2e/usersPage.cy.js
+++ cypress/e2e/usersPage.cy.js
@@ -4,6 +4,8 @@ describe('Users Page Tests', () => {
     cy.url().should('include', '/users')
 
     cy.get('.loading').should('be.visible');
+
+    cy.get('.logo').should('be.visible');
   });
 
   it('should have an export csv button', () => {
@@ -48,5 +50,34 @@ describe('Users Page Tests', () => {
     cy.get('[data-cy=users-table]').scrollTo('bottom');
     cy.get('.loading').should('be.visible');
 
+  });
+
+  it('Mobile view tests', () => {
+
+    cy.visit('/');
+    cy.viewport('samsung-note9');
+    cy.get('.logo').should('be.visible');
+
+    cy.get('[data-cy=export-csv]').contains('Export Csv File').should('be.visible');
+
+    cy.get('[data-cy=filter-by-gender').should('be.visible');
+
+    cy.get('[data-cy=filter-by-nationality]').should('be.visible');
+
+    cy.get('[data-cy=select-table-column]').should('be.visible');
+
+    cy.get('[data-cy=users-table]').should('be.visible');
+
+    cy.get('.menu-icon').should('be.visible');
+    cy.get('.menu-icon').click();
+
+    cy.get('.close-icon').should('be.visible');
+    cy.get('.avatar').should('be.visible');
+    cy.get('.name').should('be.visible');
+    cy.get('.menu-button').should('be.visible');
+
+    cy.get('.close-icon').click();
+
+    cy.get('.menu-icon').should('be.visible');
   });
 })
